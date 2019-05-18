d01068c72b3e88ebe33579b6c37dd720c70bce30
Jakedalus <jacquedalus@gmail.com>
2019-05-18 15:28:14 -0400

    Initial commit

---
+++ forEach.js
@@ -0,0 +1,12 @@
+function forEach(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    callback(arr[i], i, arr);
+  }
+};
+       
+var arr = [1, 2, 3];
+
+forEach(arr, function(num, index, array) {
+  console.log(num, index, array);
+});
+      
